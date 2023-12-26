a625bfa86a926631b5c39986dfaa8e7ec1819357
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-12-27 04:05:17 +0700

    add: mobile sidebar with blur bg

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import DropDown from "./DropDown";
-import { PiShoppingCart } from "react-icons/pi";
+import { PiShoppingCart, PiXSquare } from "react-icons/pi";
 import { RiBarChartHorizontalFill } from "react-icons/ri";
+import { useState } from "react";
+import CustomAccordion from "./CustomAccordion";
 
 const collections = [
   {
@@ -30,32 +34,72 @@ const shop = [
 ];
 
 const NavBar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+
   return (
-    <div className="w-full sticky top-0 z-30">
-      <nav className="px-3 sm:px-5 xl:px-12 py-5 flex justify-between items-center bg-black w-full">
-        <div className="hidden lg:flex">
-          <DropDown topic="Collections" items={collections} />
-          <DropDown topic="Shop" items={shop} />
-        </div>
-        <div className="flex lg:hidden text-primary">
-          <RiBarChartHorizontalFill className="text-[23px] md:text-[27px]" />
-        </div>
-        <Link
-          href="/"
-          passHref
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    <>
+      <div
+        className={`fixed lg:hidden top-0 h-full transition-all duration-300 z-50 ${
+          isSidebarOpen ? "w-[280px]" : "w-0"
+        } bg-neutral-900 text-white`}
+      >
+        <div
+          className="w-full h-full relative flex flex-col justify-start items-center
+          px-12 pt-28 pb-20 text-primary hidden"
         >
-          <img
-            src="/images/logo.png"
-            alt="logo"
-            className="w-[150px] lg:w-[200px] border-2 lg:border-4 border-primary"
+          <PiXSquare
+            className="absolute top-5 right-5 text-4xl cursor-pointer"
+            onClick={() => setIsSidebarOpen(false)}
           />
-        </Link>
-        <div className="text-primary">
-          <PiShoppingCart className="text-[23px] md:text-[27px] lg:text-[35px]" />
+          <CustomAccordion />
         </div>
-      </nav>
-    </div>
+      </div>
+
+      <div
+        className={`fixed lg:hidden top-0 left-0 h-full w-full z-40 ${
+          isSidebarOpen ? "scale-100" : "scale-0 transition-all delay-700"
+        }`}
+        onClick={() => setIsSidebarOpen(false)}
+      >
+        <div
+          className={`fixed lg:hidden top-0 left-0 h-full w-full z-40 ${
+            isSidebarOpen ? "opacity-100" : "opacity-0"
+          } backdrop-blur-[2.3px] transition-opacity duration-700 ease-out`}
+        />
+      </div>
+
+      <div
+        className="w-full sticky top-0 px-3 sm:px-7 xl:px-12 py-3 sm:py-5 z-30
+        bg-black flex justify-between items-center shadow-2xl"
+      >
+        <nav className="flex justify-between items-center w-full">
+          <div className="hidden md:flex">
+            <DropDown topic="Collections" items={collections} />
+            <DropDown topic="Shop" items={shop} />
+          </div>
+          <button
+            className="flex md:hidden text-primary p-1 transition-transform hover:translate-x-0.5"
+            onClick={() => setIsSidebarOpen(true)}
+          >
+            <RiBarChartHorizontalFill className="text-[23px] md:text-[27px]" />
+          </button>
+          <Link
+            href="/"
+            passHref
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          >
+            <img
+              src="/images/logo.png"
+              alt="logo"
+              className="w-[150px] lg:w-[200px] border-2 lg:border-4 border-primary"
+            />
+          </Link>
+          <div className="text-primary">
+            <PiShoppingCart className="text-[23px] md:text-[27px] lg:text-[35px] cursor-pointer" />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
--- client/app/components/ShopSection.tsx
+++ client/app/components/ShopSection.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const ShopSection = () => {
   return (
-    <div className="w-full pt-7 pb-2 xl:pb-3 bg-white">
-      <div className="w-full flex flex-col sm:flex-row gap-2 xl:gap-3 sm:h-[29vw]">
+    <div className="w-full pt-7 pb-1 sm:pb-2 xl:pb-3 bg-white">
+      <div className="w-full flex flex-col sm:flex-row gap-1 sm:gap-2 xl:gap-3 sm:h-[29vw]">
         <Link
           href="/shop/music"
           passHref
@@ -13,7 +13,7 @@ const ShopSection = () => {
             className="w-full bg-[url('/images/shop2.png')] bg-cover h-full
             transition-transform duration-200 hover:scale-110"
           />
-          <div className="absolute left-7 bottom-10 text-primary font-bold uppercase">
+          <div className="absolute left-7 bottom-5 lg:bottom-10 text-primary font-bold uppercase">
             <div className="text-sm lg:text-lg">shop all</div>
             <div className="text-2xl sm:text-4xl 2xl:text-5xl">music</div>
           </div>
@@ -27,7 +27,7 @@ const ShopSection = () => {
             className="w-full bg-[url('/images/shop1.png')] bg-cover h-full
             transition-transform duration-200 hover:scale-110"
           />
-          <div className="absolute left-7 bottom-10 text-primary font-bold uppercase">
+          <div className="absolute left-7 bottom-5 lg:bottom-10 text-primary font-bold uppercase">
             <div className="text-sm lg:text-lg">shop all</div>
             <div className="text-2xl sm:text-4xl 2xl:text-5xl">apparel</div>
           </div>
